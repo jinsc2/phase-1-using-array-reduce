39f1df2011f36591d2e6cfb826c90d4f7a989e27
Jinwoong <jinsc2@gmail.com>
2023-02-10 15:21:53 -0500

    commit tree 168d25b0a1ee73c6a4de625d84d4b77ad56942be

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// const totalBatteries = batteryBatches.reduce(function(count, currentBattery){
+//     if (currentBattery in count){
+//         count[currentBattery]++;
+        
+//     } 
+//     return count;});
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator})
+// => 11
